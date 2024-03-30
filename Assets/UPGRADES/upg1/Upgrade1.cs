3fc7c611d47cc15759b857a1f8438ff93d0eb4aa
xCuzMe12 <rapematic12@gmail.com>
2024-03-30 11:13:01 +0100

    UPG SYSTEM KONCAN

--- Assets/UPGRADES/upg1/Upgrade1.cs
+++ Assets/UPGRADES/upg1/Upgrade1.cs
@@ -13,16 +13,20 @@ public class Upgrade1 : MonoBehaviour
 
     public void DvaVzporedno() //11
     {
-        puscica.GetComponent<Shooting>().upgtype = 11;
+        puscica.GetComponent<Shooting>().upgtype = "11";
     }
 
     public void TriNaprej() //12
     {
-        puscica.GetComponent<Shooting>().upgtype = 12;
+        puscica.GetComponent<Shooting>().upgtype = "12";
     }
 
     public void DvaZaporedno() //13
     {
-        puscica.GetComponent<Shooting>().upgtype = 13;
+        puscica.GetComponent<Shooting>().upgtype = "13";
+    }
+    public void DvaNazaj() //14
+    {
+        puscica.GetComponent<Shooting>().upgtype = "14";
     }
 }
