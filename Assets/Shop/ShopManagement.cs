746373ba5e656a25c9c120b41cdb655291127810
xCuzMe12 <rapematic12@gmail.com>
2024-03-31 19:37:15 +0200

    shop3

--- Assets/Shop/ShopManagement.cs
+++ Assets/Shop/ShopManagement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ShopManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int speed = 0;
+    public int health = 0;
+    public int attackSpeed = 0;
+    public int bodyDMG = 0;
+    public int DMG = 0;
+    public int bulletSpeed = 0;
+    public int bulletPen = 0;
+
+
+
+    void speedUpg()
     {
-        
+        speed++;
+    }
+    void healthUpg()
+    {
+        health++;
+    }
+    void attackSpeedUpg()
+    {
+        attackSpeed++;
+    }
+    void bodyDMGUpg()
+    {
+        bodyDMG++;
+    }
+    void DMGUpg()
+    {
+        DMG++;
+    }
+    void bulletSpeedUpg()
+    {
+        bulletSpeed++;
+    }
+    void bulletPenUpg()
+    {
+        bulletPen++;
     }
 }
